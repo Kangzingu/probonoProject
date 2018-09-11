f79191f9b43f3f0214f85c238979e55399ba86fd
RessChild <wpqldmlrna@naver.com>
2018-09-11 19:36:07 +0900

    스테이지 2 코드추가
    차 움직이기, 사람 움직이기, 제너레이터

---
+++ 18_08_13/Assets/Scripts/Stage2/BCarMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BCarMove : MonoBehaviour {
+
+    GameObject Car;
+    int state;
+
+	// Use this for initialization
+	void Start () {
+        Car = GameObject.Find("Car");
+        state = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch (state)
+        {
+            case 0:
+                Car.transform.Translate(0, 0, 1.0f); 
+                break;
+            case 1:
+                
+                break;
+        }
+        // 차량들 이동
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
